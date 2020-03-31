055348e9e17a3ec143e2bdbda4cbf324609abfd5
helixious <helixious@protonmail.com>
2020-03-30 18:52:34 -0700

    [ref] workflows
    downgrade cdt to v1.6.3

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,15 @@ FROM helixious86/nodejs_base_image:latest
 USER root
 
 # Arguments that may be overridden by the user
-ARG release=latest
+ARG cdt_release=v1.6.3
+ARG eos_release=latest
 
 # Install required packages
 RUN apt-get update && apt install libusb-1.0-0
 
 # Install CDT & EOS from deb package
 ADD install_deb.sh /
-RUN chmod +rx install_deb.sh && ./install_deb.sh $release && rm -f install_deb.sh
+RUN chmod +rx install_deb.sh && ./install_deb.sh $cdt_release $eos_release && rm -f install_deb.sh
 
 # build system contracts
 ADD build_contracts.sh /
--- install_deb.sh
+++ install_deb.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-	echo "In order to continue, you must specify either latest or the release version."
+	echo "In order to continue, you must specify either latest or the release version for cdt."
 	exit 1
 elif [ "$1" = "latest" ]; then
 	cdt=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest | grep "browser_download_url.*deb" | cut -d '"' -f 4)
 	cdt_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest | grep "name.*deb" | cut -d '"' -f 4)
-    eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "browser_download_url.*18.04" | cut -d '"' -f 4)
-    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "name.*18.04" | cut -d '"' -f 4)
+
 else
 	cdt=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/tags/$1 | grep "browser_download_url.*deb" | cut -d '"' -f 4)
 	cdt_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/tags/$1 | grep "name.*deb" | cut -d '"' -f 4)
-    eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/$1 | grep "browser_download_url.*18.04" | cut -d '"' -f 4)
-    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/$1 | grep "name.*18.04" | cut -d '"' -f 4)
+
+fi
+
+if [ "$2" = "" ]; then
+	echo "In order to continue, you must specify either latest or the release version for eosio."
+	exit 1
+elif [ "$2" = "latest" ]; then
+	eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "browser_download_url.*18.04" | cut -d '"' -f 4)
+    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "name.*18.04" | cut -d '"' -f 4)
+else 
+    eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/$2 | grep "browser_download_url.*18.04" | cut -d '"' -f 4)
+    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/$2 | grep "name.*18.04" | cut -d '"' -f 4)
 fi
 
 if [ "$cdt" = "" ]; then
-	echo "Either $1 is not a valid release, or there is not a published .deb package for the release."
+	echo "Either $1 is not a valid release, or there is not a published .deb package for the cdt release."
+	exit 1
+fi
+
+if [ "$eos" = "" ]; then
+	echo "Either $2 is not a valid release, or there is not a published .deb package for the eos release."
 	exit 1
 fi
 
