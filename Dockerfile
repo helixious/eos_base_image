45d6014b615001f4dec84fb1b6b0090cae05bb2c
helixious <helixious@protonmail.com>
2020-05-30 15:21:10 -0700

    forked centos
    init

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,10 @@
-FROM helixious86/ubuntu_base_image:latest
+# FROM helixious86/ubuntu_base_image:latest
+FROM helixious86/eos_base_image:centos7.v1.0
+# FROM eostudio/eosio.cdt:v1.7.0
 USER root
-
-# Arguments that may be overridden by the user
-ARG cdt_release=latest
+ARG cdt_release=v1.6.3
 ARG eos_release=latest
+ARG contract_release=release/1.8.x
 
-# Install required packages
-RUN apt-get update && apt install libusb-1.0-0
-
-# Install CDT & EOS from deb package
-ADD install_deb.sh /
-RUN chmod +rx install_deb.sh && ./install_deb.sh $cdt_release $eos_release && rm -f install_deb.sh
-
-# build system contracts
-ADD build_contracts.sh /
-RUN chmod +rx build_contracts.sh
-
-# ENTRYPOINT service ssh restart && bash
+RUN mkdir -p /contracts
+RUN chmod +x install_deb.sh && ./install_deb.sh $cdt_release $eos_release $contract_release && rm -f install_deb.sh
--- install_deb.sh
+++ install_deb.sh
@@ -4,12 +4,12 @@ if [ "$1" = "" ]; then
 	echo "In order to continue, you must specify either latest or the release version for cdt."
 	exit 1
 elif [ "$1" = "latest" ]; then
-	cdt=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest | grep "browser_download_url.*deb" | cut -d '"' -f 4)
-	cdt_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest | grep "name.*deb" | cut -d '"' -f 4)
+	cdt=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest | grep "browser_download_url.*rpm" | cut -d '"' -f 4)
+	cdt_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/latest | grep "name.*rpm" | cut -d '"' -f 4)
 
 else
-	cdt=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/tags/$1 | grep "browser_download_url.*deb" | cut -d '"' -f 4)
-	cdt_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/tags/$1 | grep "name.*deb" | cut -d '"' -f 4)
+	cdt=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/tags/$1 | grep "browser_download_url.*rpm" | cut -d '"' -f 4)
+	cdt_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eosio.cdt/releases/tags/$1 | grep "name.*rpm" | cut -d '"' -f 4)
 
 fi
 
@@ -17,12 +17,18 @@ if [ "$2" = "" ]; then
 	echo "In order to continue, you must specify either latest or the release version for eosio."
 	exit 1
 elif [ "$2" = "latest" ]; then
-	eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "browser_download_url.*18.04" | cut -d '"' -f 4)
-    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "name.*18.04" | cut -d '"' -f 4)
+	eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "browser_download_url.*rpm" | cut -d '"' -f 4)
+    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/latest | grep "name.*rpm" | cut -d '"' -f 4)
 else 
-    eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/tags/$2 | grep "browser_download_url.*18.04" | cut -d '"' -f 4)
-    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/tags/$2 | grep "name.*18.04" | cut -d '"' -f 4)
+    eos=$(curl -s https://api.github.com/repos/EOSIO/eos/releases/tags/$2 | grep "browser_download_url.*rpm" | cut -d '"' -f 4)
+    eos_filename=$(/usr/bin/curl -s https://api.github.com/repos/EOSIO/eos/releases/tags/$2 | grep "name.*rpm" | cut -d '"' -f 4)
 fi
+
+if [ "$3" = "" ]; then
+	echo "In order to continue, you must specify either latest or the release version for contracts."
+	exit 1
+fi
+
 
 if [ "$cdt" = "" ]; then
 	echo "Either $1 is not a valid release, or there is not a published .deb package for the cdt release."
@@ -32,17 +38,24 @@ fi
 if [ "$eos" = "" ]; then
 	echo "Either $2 is not a valid release, or there is not a published .deb package for the eos release."
 	exit 1
+else
+	contract_release="$3"
 fi
 
-/usr/bin/wget $eos && /usr/bin/dpkg -i "$eos_filename" && rm -f "$eos_filename"
-/usr/bin/wget $cdt && /usr/bin/dpkg -i "$cdt_filename" && rm -f "$cdt_filename"
+wget $eos && yum install -y $eos_filename && rm -rf "$eos_filename"
+wget $cdt && yum install -y $cdt_filename && rm -rf "$cdt_filename"
+# /usr/bin/wget $eos && /usr/bin/dpkg -i "$eos_filename" && rm -f "$eos_filename"
+# /usr/bin/wget $cdt && /usr/bin/dpkg -i "$cdt_filename" && rm -f "$cdt_filename"
 
-cd /
-git clone https://github.com/EOSIO/eosio.contracts.git
-cd ./eosio.contracts/
+# sleep 10
+
+cd /contracts
+git clone -b $contract_release https://github.com/EOSIO/eosio.contracts.git
+cd ./eosio.contracts
+sleep 10
 ./build.sh -y
 
-mkdir /contracts
 mkdir /contracts/eosio
 cp /eosio.contracts/build/contracts/. /contracts/eosio -a
-rm -rf /eosio.contracts
+rm -rf /eosio.contracts-1.8.x/
+
